266d51a179086eb0f8ca80a1154a62426d10d125
skydancerbg <13396312+skydancerbg@users.noreply.github.com>
2026-06-29 15:33:01 +0000

    Phase 5.16: login dark bg, judge nav, admin back-link, card borders

--- scoreboard/scoreboard/urls.py
+++ scoreboard/scoreboard/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+admin.site.site_url = '/contest/'   # "View Site" in admin header → judge competitions list
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
